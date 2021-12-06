dfd8172af4f51b59d230261e92f3a99be5d24de8
yand1960 <yand1960@gmail.com>
2021-12-06 11:24:57 +0300

    YA

--- SP01_DI/src/main/java/my/animals1/Process.java
+++ SP01_DI/src/main/java/my/animals1/Process.java
@@ -1,11 +1,22 @@
 package my.animals1;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Process {
 
     public static void main(String[] args) {
         //При POJO тесная зависимость неизбежна
-        Animal a = new Cat();
+//        Animal a = new Cat();
+//        a.eat();
+
+        Animal a;
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config1.xml");
+
+        a = context.getBean(Animal.class);
+        System.out.println(a.voice());
         a.eat();
+
     }
 
 }
