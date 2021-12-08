05f7c454714f9a359199f5a7176154da42179925
yand <yand@outlook.com>
2021-12-08 11:48:03 +0300

    YA

--- SP04_REST/src/main/java/my/rest/ProductController.java
+++ SP04_REST/src/main/java/my/rest/ProductController.java
@@ -1,8 +1,16 @@
 package my.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class ProductController {
+
+    @RequestMapping("api/time")
+    public String time() {
+        return (new Date()).toString();
+    }
 
 }
