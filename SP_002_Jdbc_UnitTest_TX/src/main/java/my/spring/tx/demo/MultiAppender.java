f7a74a400e4e89c65de33c30606f4245c8cbb850
yand <yand@outlook.com>
2021-12-23 18:16:22 +0300

    Ya

--- SP_002_Jdbc_UnitTest_TX/src/main/java/my/spring/tx/demo/MultiAppender.java
+++ SP_002_Jdbc_UnitTest_TX/src/main/java/my/spring/tx/demo/MultiAppender.java
@@ -2,6 +2,7 @@ package my.spring.tx.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class MultiAppender {
@@ -9,10 +10,11 @@ public class MultiAppender {
     @Autowired
     DataAppender appender;
 
+    @Transactional
     public void append3() {
         appender.append("YA1");
         appender.append("YA2");
-        if ( 1 == 1)
+        if (1 == 0)
             throw new RuntimeException("TEST EXCEPTION");
         appender.append("YA3");
     }
