435626be02b3a534da4e13b788dd14b6b7c5adc0
yand <yand@outlook.com>
2021-12-28 17:10:52 +0300

    YA

--- SP_005_Web/src/main/java/my/spring/rest/TimeController.java
+++ SP_005_Web/src/main/java/my/spring/rest/TimeController.java
@@ -10,6 +10,11 @@ public class TimeController {
 
     @RequestMapping("api/time")
     public String time() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return (new Date()).toString();
     }
 
